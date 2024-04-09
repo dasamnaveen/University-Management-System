58e2d15bcc6e7367cd695976866dd3e1e7766935
Dasam Naveen Srinivas <124710210+dasamnaveen@users.noreply.github.com>
2024-04-10 02:44:41 +0530

    Add files via upload

---
+++ UMS/timeTable.js
@@ -0,0 +1,173 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '227',
+        subject: 'CSEN2061',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '224',
+        subject: 'CSEN3081',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '227',
+        subject: 'CSEN3071',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '227',
+        subject: 'OPTS6021',
+        type: 'Lecture'
+    },
+    {
+        time: '2-4 PM',
+        roomNumber: '510',
+        subject: 'CSEN2061P',
+        type: 'Practical'
+    }
+]
+const Tuesday =[
+    {   
+        time: '08-10 AM',
+        roomNumber: '319',
+        subject: 'CSEN2091P',
+        type: 'Practical'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '227',
+        subject: 'CSEN3061',
+        type: 'Lecture'
+    },
+    {
+        time: '11-12 AM',
+        roomNumber: '227',
+        subject: 'CSEN2071',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '224',
+        subject: 'CSEN3081',
+        type: 'Lecture'
+    },
+    {
+        time: '02-04 PM',
+        roomNumber: '228',
+        subject: 'CSEN3261P',
+        type: 'Practical'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '224',
+        subject: 'CSEN3081',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '224',
+        subject: 'CSEN2091',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '227',
+        subject: 'CSEN3261',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '227',
+        subject: 'OPTS6021',
+        type: 'Lecture'
+    },
+    {
+        time: '2-4 PM',
+        roomNumber: '127',
+        subject: 'INTN2333',
+        type: 'Practical'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '227',
+        subject: 'CSEN2091',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '227',
+        subject: 'CSEN3071',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '227',
+        subject: 'CSEN3071',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '227',
+        subject: 'CSEN2061',
+        type: 'Lecture'
+    },
+    {
+        time: '12-01 PM',
+        roomNumber: '510',
+        subject: 'CSEN3261',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '227',
+        subject: 'CSEN3261',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '224',
+        subject: 'CSEN2091',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '405',
+        subject: 'OPTS6021',
+        type: 'Lecture'
+    },
+    {
+        time: '12-01 PM',
+        roomNumber: '510',
+        subject: 'CSEN3071P',
+        type: 'Practical'
+    }
+]
+const Saturday =[
+    {    
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
